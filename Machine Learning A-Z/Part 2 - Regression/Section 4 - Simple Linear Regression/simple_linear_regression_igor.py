f9551becede0f743555902446d8261c5fc65a2a6
Igor Busquets <igor.busquets@altran.com>
2017-11-29 00:02:02 +0000

    fit linear regression and prediction of future values

--- Machine Learning A-Z/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression_igor.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression_igor.py
@@ -1,6 +1,50 @@
-# Simple Linear Regression
+# Data Preprocessing Template
 
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+from sklearn.linear_model import LinearRegression
+####DATA PREPROCESSING
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+
+#Matrix of Features X independent variable is the Salary
+X = dataset.iloc[:, :-1].values
+
+#Vector Y - dependent variable Experience
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+
+#test_size = 1/3 = 10 in total for testset
+#random_state = 0 so the results are the same from the tutorial
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+# Feature Scaling
+#libs does the job and data preprocessing is finished
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+#####FIT LINEAR REGRESSION TO TRAINING SET
+#The models learns the correlation from the training set
+#from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+######Predicting the testset results
+#vector of predict 
+#value of predicted salaries
+#X is matrix of features
+#y_pred is convention for naming the future desirable(independent variable) value
+y_pred = regressor.predict(X_test)
+
+
+
+
