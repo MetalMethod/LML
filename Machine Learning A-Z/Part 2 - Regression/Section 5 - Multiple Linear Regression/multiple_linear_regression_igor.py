1cc1bb1353c8af732ee977eb9de5296106b803be
Igor Busquets <igor.busquets@altran.com>
2017-12-02 17:11:09 +0000

    multiple linear regression data preprocessing done

--- Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression_igor.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression_igor.py
@@ -10,12 +10,31 @@ Created on Fri Dec  1 15:07:50 2017
 import numpy
 import matplotlib.pyplot as plt
 import pandas
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
 
 ###### Data Preprocessing
 # Importing the dataset
 dataset = pandas.read_csv('50_Startups.csv')
-#X = dataset.iloc[:, :-1].values
-#y = dataset.iloc[:, 3].values
+#matrix of features - dependent variables
+# columns 0 to 3
+X = dataset.iloc[:, :-1].values
+
+#independent variable 
+y = dataset.iloc[:, 4].values
+
+
+#ENCODE CATEGORICAL DATA - Encode data so there are no string columns
+label_encoder_X = LabelEncoder()
+# encode the first column
+X[:,3] = label_encoder_X.fit_transform(X[:, 3])
+# transform the encoded column into 3 different columns populated with 1 and 0 only
+one_hot_encoder = OneHotEncoder(categorical_features = [3])
+X = one_hot_encoder.fit_transform(X).toarray() 
+
+#AVOIDING DUMMY TRAP
+#remove the first column of X, one of the dummy encoded 
+X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
@@ -28,5 +47,6 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
 
 
