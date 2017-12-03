27e9e7ac7ba1a2bdac9edba64a0693412c239049
Igor Busquets <igor.busquets@altran.com>
2017-12-03 23:19:33 +0000

    Decision Tree Regression model finished with high res plot.

--- Machine Learning A-Z/Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression_igor.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression_igor.py
@@ -4,7 +4,53 @@ Created on Sun Dec  3 22:53:08 2017
 @author: Igor busquets LML
 """
 
-#Decision Tree Regression Model
+#Decision Tree Regression Model - a non continual model
+
+#######DATA PREPOCESSING
+# Regression Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+# Create your regressor here
+from sklearn.tree import DecisionTreeRegressor
+# default criteria is MSE: mean of squared error
+# random_state = 0 gets same results of the tutorial
+regressor = DecisionTreeRegressor(random_state = 0)
+
+# Fitting the Regression Model to the dataset
+regressor.fit(X, y)
+
+# Predicting a new result
+y_pred = regressor.predict(6.5)
 
 
-
+# Visualising the Regression results (for higher resolution and smoother curve)
+# 0.01 is very high resolution
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
