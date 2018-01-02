5689891c1e2c15cef16e5deeea12fe86c7144238
Igor Busquets <igor.busquets@altran.com>
2018-01-02 23:19:48 +0000

    Finished my very first steps into Reinforcement Learning: Upper Bound Confidence and Thompsom Sampling

--- Machine Learning A-Z/Part 6 - Reinforcement Learning/Section 33 - Thompson Sampling/igor_thompson_sampling.py
+++ Machine Learning A-Z/Part 6 - Reinforcement Learning/Section 33 - Thompson Sampling/igor_thompson_sampling.py
@@ -3,41 +3,57 @@ Igor Busquets LML
 """
 
 #REINFORCEMENT LEARNING: Thompsom Sampling
+# tries to estimate the highest probability of success
 
 #importing depedencies
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import math 
+import random
 ###########Importing dataset
 dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
 
-##Implementing Random Selection
-#import random
-##N = number of records
-#N = 10000
-##d = different Ads to display
-#d = 10
-#ads_selected = []
-#total_reward = 0
-#
-#for n in range(0, N):
-#    ad = random.randrange(d)
-#    ads_selected.append(ad)
-#    reward = dataset.values[n, ad]
-#    total_reward = total_reward + reward
-#    
-######VIsulalising the results - Histogram
-#plt.hist(ads_selected)
-#plt.title('Histogram of ads selections')
-#plt.xlabel('Ads')
-#plt.ylabel('Number of times each Ad was selected')
-#plt.show()
+# CHECK Upper Confidence Bound algo for the random implementation
 
-######## Evaluating Historgram:
-#total_reward = sum of the rewards of wach rounds
-# total should be compared to other algos and models for evaluation
+######### Implementing Thompsom Sampling
+N = 10000
+# d = number of elements
+d = 10 
+ads_selected = []  
 
+#vector of d elements, the number of each time a Ad is rated 1 (selected) AND rated 0 (not selected)
+numbers_of_rewards_1 = [0] * d
+numbers_of_rewards_0  = [0] * d
 
+total_reward = 0
+
+for n in range(0, N):
+    ad = 0
+    #max random  draw, the max of a selected Ad by random selections
+    max_random = 0
+    for i in range(0, d):
+        #random_beta  = different random draws 
+        #foloows the math formula of slie 2
+        random_beta = random.betavariate(numbers_of_rewards_1[i] + 1, numbers_of_rewards_0[i] + 1)
+                
+        if random_beta > max_random:
+            max_random = random_beta
+            ad = i
+    
+    ads_selected.append(ad)
+    reward = dataset.values[n,  ad]
+    
+    #increment the rewards
+    if reward == 1:
+        numbers_of_rewards_1[ad] = numbers_of_rewards_1[ad] + 1 
+    else:
+        numbers_of_rewards_0[ad] = numbers_of_rewards_0[ad] + 1 
+    # total_reward  = measure of the algorith (almotst 25% better than UCB, more than a double of random selection)
+    total_reward = total_reward + reward
         
-        
+#####VIsulalising the results - Histogram
+plt.hist(ads_selected)
+plt.title('Histogram of ads selections')
+plt.xlabel('Ads')
+plt.ylabel('Number of times each Ad was selected')
+plt.show()
