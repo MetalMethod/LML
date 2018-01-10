fe8ba459de10a0ac28f726472e23531d01b71d71
Igor Busquets <igor.busquets@altran.com>
2018-01-10 20:55:33 +0000

    Artifficial Neural Network: wrote the steps that I think will be taken without looking at tutorial

--- Machine Learning A-Z/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/igor_artificial_neural_network.py
+++ Machine Learning A-Z/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/igor_artificial_neural_network.py
@@ -5,3 +5,16 @@ Igor Busquets LML
 
 #Artifficial Neural Networks
 
+#what I think is needed:
+
+# Data pre processing
+# a array of features X
+# add Random weights
+# Activation Function (relu or sigmoid)
+# output to ^y with sigmoid
+# feed hidden layers and repeat until reaching output y
+
+# calculate Error and Cost function
+# backpropagation and update weights
+
+# run again for all the rows until Epoch.
