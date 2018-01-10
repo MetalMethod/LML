f6613ccf3db31246b84faf4a98ef0cbd5734095a
Igor Busquets <igor.busquets@altran.com>
2018-01-10 22:32:21 +0000

    Spent 2 hours trying to install TensorFlow on windows when I realised I wasnt running conda as a administrator...

--- Machine Learning A-Z/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/igor_artificial_neural_network.py
+++ Machine Learning A-Z/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/igor_artificial_neural_network.py
@@ -8,6 +8,7 @@ Igor Busquets LML
 #what I think is needed:
 
 # Data pre processing
+#feature scaling
 # a array of features X
 # add Random weights
 # Activation Function (relu or sigmoid)
@@ -18,3 +19,16 @@ Igor Busquets LML
 # backpropagation and update weights
 
 # run again for all the rows until Epoch.
+
+
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
+
+# Installing Keras
+# 
+import tensorflow as af
+import theano as th
+import keras
