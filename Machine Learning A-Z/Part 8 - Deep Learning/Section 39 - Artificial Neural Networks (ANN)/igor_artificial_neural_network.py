ad87af2ff7de19eb804083ea20b93a81ecbd41a1
Igor Busquets <igor.busquets@altran.com>
2018-01-10 22:58:54 +0000

    Artifficial Neural Network - Data preprocessing finished

--- Machine Learning A-Z/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/igor_artificial_neural_network.py
+++ Machine Learning A-Z/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/igor_artificial_neural_network.py
@@ -1,10 +1,10 @@
-
 """
 Igor Busquets LML
 """
 
 #Artifficial Neural Networks
 
+################################################################
 #what I think is needed:
 
 # Data pre processing
@@ -19,16 +19,54 @@ Igor Busquets LML
 # backpropagation and update weights
 
 # run again for all the rows until Epoch.
+################################################################
 
-
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
-
-# Installing Keras
-# 
+# DEPENDENCIES
 import tensorflow as af
 import theano as th
-import keras
+import keras as kr
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# DATA PREPROCESSING
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+#Feature Encoding
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+# Enconding Country Column
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+# Enconding Gender Column
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+#create dummy variables
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+#Avoid dummy variables trap
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+# Fitting classifier to the Training set
+# Create your classifier here
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
