51c4871ef0a779d24622455ea0685ee67b5f9deb
Igor Busquets <igor.busquets@altran.com>
2018-01-11 23:07:01 +0000

    Finished implementation of Artifficial Neural Network with training of classifier and accuracy of 86% in a Business problem (if a client leavesor not)

--- Machine Learning A-Z/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/igor_artificial_neural_network.py
+++ Machine Learning A-Z/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/igor_artificial_neural_network.py
@@ -4,31 +4,12 @@ Igor Busquets LML
 
 #Artifficial Neural Networks
 
-################################################################
-#what I think is needed:
-
-# Data pre processing
-#feature scaling
-# a array of features X
-# add Random weights
-# Activation Function (relu or sigmoid)
-# output to ^y with sigmoid
-# feed hidden layers and repeat until reaching output y
-
-# calculate Error and Cost function
-# backpropagation and update weights
-
-# run again for all the rows until Epoch.
-################################################################
-
 # DEPENDENCIES
-import tensorflow as af
-import theano as th
-import keras
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+import tensorflow as af
 
 # DATA PREPROCESSING
 # Importing the dataset
@@ -45,7 +26,7 @@ X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
 labelencoder_X_2 = LabelEncoder()
 X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
 #create dummy variables
-onehotencoder = OneHotEncoder(categorical_features = [0])
+onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
 #Avoid dummy variables trap
 X = X[:, 1:]
@@ -87,33 +68,24 @@ classifier.add(Dense(output_dim = 1, init='uniform', activation = 'sigmoid'))
 #metrics = crteria for evaluate model, criteria is accuracy...a array is expected
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'] )
 
+# TRAINING
+#Fitting the ANN to traningset
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
 
-#Initializes random weights close to 0
-
-#Input layer will have 11 input nodes
-
-#Activation function Relu
-
-#Output function sigmoid
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# PREDICTIONS
-# Predicting the Test set results
+# PREDICTION
 y_pred = classifier.predict(X_test)
+
+#change predictions to 1 or 0 values with a treshold
+y_pred = (y_pred  > 0.5)
+
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+#accuracy
+accuracy = (1509 + 202) / 2000
+
+#short evalualion, returns [loss_func, accuracy]
+classifier.evaluate(X_train, y_train, batch_size=10, verbose=1)
+classifier.summary()
