48fb3df8448d2219983440759e2a9f3f94570678
Igor Busquets <igor.busquets@altran.com>
2018-01-23 23:17:18 +0000

    Model selection: k-Fold Cross Validation - finished.

---
+++ Machine Learning A-Z/Part 10 - Model Selection & Boosting/Section 48 - Model Selection/igor_k-fold_cross_validation.py
@@ -0,0 +1,108 @@
+"""
+Igor Busquets LML
+"""
+
+# k-Fold Cross Validation
+
+#improve model 
+#model selection = best params for a model
+#params learnt and params chosen (hyperparams)
+
+#split test / train set - evaluates correctly but has the variance problem
+
+#different accuracy for different test sets
+#so to evaluate avoid one test set only and use k-fold Cross validation
+
+#### k-fold fixes variance problem by:
+#divide the dataset in 10 parts (iterations)
+# split each iteration into 10(usually) folds
+# train it in 9 folds, then test in one fold 
+# each selected test fold is at a different position in the iteration
+
+###########
+# Kernel SVM
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Fitting Kernel SVM to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'rbf', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+#################################
+#####PERFOMANCE EVALUATION
+#Applying k-fold across validation
+from sklearn.model_selection import cross_val_score
+
+#will return 10 accuracys of each 10 combinations of test selst from k-Fold
+#accuracy vector of 10 accuracies
+#cv = number of folds
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train,  cv = 10)
+#mean of all accuracies for overal evaluation
+accuracies.mean()
+
+#standard deviation = high variance or low variance
+accuracies.std()
+#################################
+
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Kernel SVM (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Kernel SVM (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
